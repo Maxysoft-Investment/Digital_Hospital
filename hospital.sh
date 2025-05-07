383fdd532a0abb0a4aa7b266ac18ae4cca369e41
maxysoft <nwadochelilouis@yahoo.com>
2025-05-07 22:44:34 +0100

    project initiation

---
+++ hospital.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "This is my hospital management app"
+
+echo "Find the list of our health professionals"
+echo "Doctors"
+echo "Nurses"
+echo "Lab Technicians
+echo "----------------------------------------------------"
+
+#The client visits the hospital. Nurse registers the client and gives him an ID and takes his vital signs.
+#The patient goes to the lab. The lab sends the patient's test result to the doctor and nurses.
+#The doctor examines the patient and gives treatment.
+#The patient is given a bed and all that ...
+
+
+
+
